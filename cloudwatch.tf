ac2eed9145f6b93e03fc43328137ed48bcda675b
Mikhail Usik <usik@voximplant.com>
2022-11-07 19:55:20 +0300

    switched to latest terraform-aws-modules/lambda/aws module

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -7,13 +7,13 @@ resource "aws_cloudwatch_event_rule" "certbot_lambda_timer" {
 # Specify the lambda function to run
 resource "aws_cloudwatch_event_target" "lets_encrypt_timer_target" {
   rule = aws_cloudwatch_event_rule.certbot_lambda_timer.name
-  arn  = module.certbot_lambda.function_arn
+  arn  = module.certbot_lambda.lambda_function_arn
 }
 
 # Give cloudwatch permission to invoke the function
 resource "aws_lambda_permission" "permission" {
   action        = "lambda:InvokeFunction"
-  function_name = module.certbot_lambda.function_name
+  function_name = module.certbot_lambda.lambda_function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.certbot_lambda_timer.arn
 }
